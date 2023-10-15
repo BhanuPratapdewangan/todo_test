d1cd72703ed675d5ffb81fe82e9b38d85fb03c60
bh@nu77 <bhanupratap04123@gmail.com>
2023-10-15 15:27:19 +0530

    refactor : add a new .env, add a mongodb cloud url

--- backend/db/config.js
+++ backend/db/config.js
@@ -1,7 +1,9 @@
 
 import mongoose from "mongoose";
 
-mongoose.connect("mongodb://localhost:27017/e-com", { useNewUrlParser: true, useUnifiedTopology: true })
+var MONGODB_URL = "mongodb+srv://bhanupratap04123:ADYB$d-uT9YeYWi@cluster0.eyfrwj5.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => {
     console.log('Connected to MongoDB');
   })
